3bf96c3a925f0058afbbebe66caba953a36181fb
PrathyushaLakkireddy <prathyusha@vitwit.com>
2020-08-06 13:51:38 +0530

    Add validator status and missed blocks alerting

--- main.go
+++ main.go
@@ -1,5 +1,43 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"sync"
+	"time"
 
+	"github.com/Chainflow/SCRT/config"
+	"github.com/Chainflow/SCRT/server"
+)
+
+func main() {
+	cfg, err := config.ReadConfigFromFile()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	// Calling go routine to send alerts for missed blocks
+	go func() {
+		for {
+			if err := server.SendSingleMissedBlockAlert(cfg); err != nil {
+				fmt.Println("Error while sending missed block alerts", err)
+			}
+			time.Sleep(4 * time.Second)
+		}
+	}()
+
+	// Calling go routine to send alert about validator status
+	go func() {
+		for {
+			if err := server.ValidatorStatusAlert(cfg); err != nil {
+				fmt.Println("Error while sending jailed alerts", err)
+			}
+			time.Sleep(60 * time.Second)
+		}
+	}()
+
+	wg.Wait()
 }
---
+++ server/missed_block_alerting.go
@@ -0,0 +1,72 @@
+package server
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"github.com/Chainflow/SCRT/config"
+)
+
+//SendSingleMissedBlockAlert to send alert when the validator miss the block
+func SendSingleMissedBlockAlert(cfg *config.Config) error {
+	log.Println("Calling missed block alerting")
+	ops := HTTPOptions{
+		Endpoint: cfg.ExternalRPC + "/status",
+		Method:   "GET",
+	}
+
+	resp, err := HitHTTPTarget(ops)
+	if err != nil {
+		log.Printf("Error: %v", err)
+		return err
+	}
+
+	var networkLatestBlock NetworkLatestBlock
+	err = json.Unmarshal(resp.Body, &networkLatestBlock)
+	if err != nil {
+		log.Printf("Error: %v", err)
+		return err
+	}
+
+	cbh := networkLatestBlock.Result.SyncInfo.LatestBlockHeight
+
+	resp, err = HitHTTPTarget(HTTPOptions{
+		Endpoint:    cfg.ExternalRPC + "/block",
+		QueryParams: QueryParams{"height": cbh},
+		Method:      "GET",
+	})
+	if err != nil {
+		log.Printf("Error getting details of current block: %v", err)
+		return err
+	}
+
+	var b ScrtBlockResponse
+	err = json.Unmarshal(resp.Body, &b)
+	if err != nil {
+		log.Printf("Error: %v", err)
+		return err
+	}
+
+	addrExists := false
+
+	for _, c := range b.Result.Block.LastCommit.Signatures {
+		if c.ValidatorAddress == cfg.ValidatorHexAddress {
+			addrExists = true
+		}
+	}
+
+	if !addrExists {
+		_ = SendTelegramAlert(fmt.Sprintf("SCRT validator %s missed a block at block height %s", cfg.ValidatorName, cbh), cfg)
+		log.Println("Sent missed block alerting")
+	}
+
+	// Calling function to check validator jailed status
+	err = CheckValidatorJailed(cfg)
+	if err != nil {
+		log.Printf("Error while sending jailed alerting: %v", err)
+		return err
+	}
+
+	return nil
+}
